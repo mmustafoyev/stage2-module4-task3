ab11f3a179a01134796106474e08953467713411
Muhammad Mustafoyev <135558935+mmustafoyev@users.noreply.github.com>
2024-03-18 17:30:55 +0500

    Update WordParser.java

--- task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
@@ -1,7 +1,18 @@
 package com.mjc.stage2.parser;
 
 
-public class WordParser {
+import com.mjc.stage2.entity.AbstractTextComponent;
+import com.mjc.stage2.entity.SymbolLeaf;
+import com.mjc.stage2.entity.TextComponentType;
+
+public class WordParser extends AbstractTextParser {
+    @Override
+    public void parse(AbstractTextComponent abstractTextComponent, String string) {
+        char[] chars = string.toCharArray();
+        for (char c : chars) {
+            abstractTextComponent.add(new SymbolLeaf(c, TextComponentType.WORD));
+        }
+    }
 
     // Write your code here!
 
