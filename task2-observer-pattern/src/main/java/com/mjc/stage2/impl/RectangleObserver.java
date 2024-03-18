f20f8d54ab79873a38ba018d44ec295fb244bf22
Muhammad Mustafoyev <135558935+mmustafoyev@users.noreply.github.com>
2024-03-18 17:25:39 +0500

    Update RectangleObserver.java

--- task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
@@ -1,5 +1,30 @@
 package com.mjc.stage2.impl;
 
+import com.mjc.stage2.Observer;
+import com.mjc.stage2.entity.Rectangle;
+import com.mjc.stage2.entity.RectangleValues;
+import com.mjc.stage2.event.RectangleEvent;
+import com.mjc.stage2.repository.RectangleRepository;
+import com.mjc.stage2.warehouse.RectangleWarehouse;
+
+
 public class RectangleObserver  {
+    @Override
+    public void handleEvent(RectangleEvent event) {
+        Rectangle rectangle = event.getSource();
+        int id = rectangle.getId();
+        double sideA = rectangle.getSideA();
+        double sideB = rectangle.getSideB();
+        double square = sideA * sideB;
+        double perimeter = 2 * (sideA + sideB);
+        RectangleValues old = RectangleWarehouse.getInstance().get(id);
+
+        RectangleWarehouse.getInstance().remove(id, old);
+        RectangleValues newValues = new RectangleValues(square, perimeter);
+        RectangleWarehouse warehouse = RectangleWarehouse.getInstance();
+        warehouse.get(id).setSquare(square);
+        warehouse.get(id).setPerimeter(perimeter);
+        warehouse.put(id, newValues);
+    }
     // Write your code here!
 }
