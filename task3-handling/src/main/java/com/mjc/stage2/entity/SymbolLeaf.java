c3252ddeb730c4aef22e39a4195de22ba6a93969
Muhammad Mustafoyev <135558935+mmustafoyev@users.noreply.github.com>
2024-03-18 17:27:04 +0500

    Update SymbolLeaf.java

--- task3-handling/src/main/java/com/mjc/stage2/entity/SymbolLeaf.java
+++ task3-handling/src/main/java/com/mjc/stage2/entity/SymbolLeaf.java
@@ -1,8 +1,33 @@
 package com.mjc.stage2.entity;
 
-public class SymbolLeaf {
+public class SymbolLeaf extends AbstractTextComponent {
 
     private char value;
+
+    public SymbolLeaf(char value, TextComponentType componentType) {
+        super(componentType);
+        this.value = value;
+    }
+
+    @Override
+    public String operation() {
+        return String.valueOf(value);
+    }
+
+    @Override
+    public void add(AbstractTextComponent textComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void remove(AbstractTextComponent textComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getSize() {
+        throw new UnsupportedOperationException();
+    }
 
     // Write your code here!
 }
