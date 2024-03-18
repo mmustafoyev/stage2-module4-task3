ed5a22884a30d86204cf9d21bfaa6f76fa49b4c3
Muhammad Mustafoyev <135558935+mmustafoyev@users.noreply.github.com>
2024-03-18 17:23:02 +0500

    Update MaxPriceFilteringStrategy.java

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/impl/MaxPriceFilteringStrategy.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/impl/MaxPriceFilteringStrategy.java
@@ -4,8 +4,13 @@ package com.mjc.stage2.impl;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public class MaxPriceFilteringStrategy {
+public class MaxPriceFilteringStrategy implements FilteringStrategy {
     private double maxPrice;
 
+    @Override
+    public boolean filter(Product product) {
+        return maxPrice >= product.getPrice();
+    }
+    
     // Write your code here!
 }
